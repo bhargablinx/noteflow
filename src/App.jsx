90d873011f22ca82abc54a463c8569a255d2b19f
bhargablinx <bhargabbhuyan001@gmail.com>
2026-04-16 14:16:39 +0530

    Conditionaly render layout1 or layout2

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Layout1 from "./layout/Layout1";
+import Layout2 from "./layout/Layout2";
 
 function App() {
+    const [selectedNote, setSelectedNote] = useState(null);
+
     return (
         <div className="h-screen flex flex-col">
             {/* Header */}
@@ -15,7 +18,17 @@ function App() {
                 <Sidebar />
 
                 {/* Main Content */}
-                <Layout1 />
+                {selectedNote ? (
+                    <Layout2
+                        selectedNote={selectedNote}
+                        onBack={() => setSelectedNote(null)}
+                        onSave={(updatedNote) => {
+                            console.log("Save logic here", updatedNote);
+                        }}
+                    />
+                ) : (
+                    <Layout1 onSelectNote={setSelectedNote} />
+                )}
             </div>
         </div>
     );

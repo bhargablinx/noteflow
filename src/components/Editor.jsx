bdba16de665c48a44c7b8314d15e2f174716b797
bhargablinx <bhargabbhuyan001@gmail.com>
2026-04-17 12:23:17 +0530

    Replace toggle with wrapToggle or toggle formating

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -7,10 +7,11 @@ export default function Editor({
     setContent,
 }) {
     function shortcutFunctionality(e) {
+        const textarea = e.target;
         const start = e.target.selectionStart;
         const end = e.target.selectionEnd;
         const before = content.substring(0, start);
-        const selected = content.substring(start, end);
+        let selected = content.substring(start, end);
         const after = content.substring(end);
 
         // Tabs Functionality
@@ -32,39 +33,85 @@ export default function Editor({
 
         if (!isCtrl) return;
 
-        const wrap = (prefix, suffix = prefix) => {
+        const toggleWrap = (prefix, suffix = prefix, placeholder = "text") => {
             e.preventDefault();
 
+            const beforePrefix = content.substring(
+                start - prefix.length,
+                start,
+            );
+            const afterSuffix = content.substring(end, end + suffix.length);
+
+            // NO SELECTION
+            if (start === end) {
+                const newText = before + prefix + placeholder + suffix + after;
+                setContent(newText);
+                setTimeout(() => {
+                    textarea.selectionStart = start + prefix.length;
+                    textarea.selectionEnd =
+                        start + prefix.length + placeholder.length;
+                }, 0);
+
+                return;
+            }
+
+            // ALREADY WRAPPED
+            if (beforePrefix === prefix && afterSuffix === suffix) {
+                const newText =
+                    content.substring(0, start - prefix.length) +
+                    selected +
+                    content.substring(end + suffix.length);
+
+                setContent(newText);
+
+                setTimeout(() => {
+                    textarea.selectionStart = start - prefix.length;
+                    textarea.selectionEnd = end - prefix.length;
+                }, 0);
+
+                return;
+            }
+
+            // WRAP
             const newText = before + prefix + selected + suffix + after;
             setContent(newText);
-
             setTimeout(() => {
-                e.target.selectionStart = start + prefix.length;
-                e.target.selectionEnd = end + prefix.length;
+                textarea.selectionStart = start + prefix.length;
+                textarea.selectionEnd = end + prefix.length;
             }, 0);
         };
 
         switch (e.key.toLowerCase()) {
             case "b":
-                wrap("**"); // bold
+                toggleWrap("**", "**", "bold");
                 break;
             case "i":
-                wrap("*"); // italic
+                toggleWrap("*", "*", "italic");
                 break;
             case "k":
-                wrap("`"); // inline code
+                toggleWrap("`", "`", "code");
                 break;
             case "s":
-                wrap("~~"); // strikethrough
+                if (e.shiftKey) {
+                    toggleWrap("~~", "~~", "strike"); // Shit + S
+                }
                 break;
             case "h":
-                wrap("=="); // highlight (need implementation in preview side!)
+                toggleWrap("==", "==", "highlight"); // highlight (need implementation in preview side!)
                 break;
             case "e":
                 // code block
                 e.preventDefault();
-                const block = `\n\`\`\`\n${selected}\n\`\`\`\n`;
+
+                const block = `\n\`\`\`\n${selected || "code"}\n\`\`\`\n`;
+
                 setContent(before + block + after);
+
+                setTimeout(() => {
+                    textarea.selectionStart = start + 5; // inside code block
+                    textarea.selectionEnd =
+                        start + 5 + (selected || "code").length;
+                }, 0);
                 break;
             default:
                 break;
